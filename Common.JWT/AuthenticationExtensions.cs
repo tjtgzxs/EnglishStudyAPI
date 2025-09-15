556e55b5ab7f1f977a3cfc2ce29c863300dabecb
tjtgzxs <tjtgzxs@gmail.com>
2025-09-15 18:25:59 +1200

    Add Jwt Commmon functions

---
+++ Common.JWT/AuthenticationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Common.JWT;
+
+public static class AuthenticationExtensions
+{
+    public static AuthenticationBuilder AddJWTAuthentication(this IServiceCollection services, JWTOptions jwtOpt)
+    {
+        return services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+            x.TokenValidationParameters = new()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidAudience = jwtOpt.Audience,
+                ValidIssuer = jwtOpt.Issuer,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOpt.Key))
+
+            });
+    }
+}
---
+++ Common.JWT/ModuleIntializar.cs
@@ -0,0 +1,12 @@
+﻿using Common.Commons;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.JWT;
+
+public class ModuleInitializer:IModuleInitializer
+{
+    public void Initialize(IServiceCollection services)
+    {
+        services.AddScoped<ITokenService, TokenService>();
+    }
+}
